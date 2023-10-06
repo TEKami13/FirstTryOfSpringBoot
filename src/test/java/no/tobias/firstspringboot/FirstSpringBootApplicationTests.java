ad7d8fb68a2276560894d1df1a5df79b47bdc26f
TEKami13 <tobias.krogh@hotmail.com>
2023-10-06 09:32:09 +0200

    Starting to make tests.

--- src/test/java/no/tobias/firstspringboot/FirstSpringBootApplicationTests.java
+++ src/test/java/no/tobias/firstspringboot/FirstSpringBootApplicationTests.java
@@ -1,13 +1,20 @@
 package no.tobias.firstspringboot;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.assertj.core.api.Assertions.assertThat;
 
 @SpringBootTest
 class FirstSpringBootApplicationTests {
 
+	@Autowired
+	private PartsController partsController;
+
 	@Test
 	void contextLoads() {
+		assertThat(partsController).isNotNull();
 	}
 
 }
